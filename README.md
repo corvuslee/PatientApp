ad2a43968e37612dc23a6b21448bb7a83dcccd67
corvuslee <corvus.lee@gmail.com>
2021-11-08 19:05:48 +0000

    Bring in the camera

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # PatientApp
 
-Based on the tutorial at https://rapidapi.com/blog/build-android-app-with-api/
+Based on the tutorials at:
+* https://rapidapi.com/blog/build-android-app-with-api/
+* https://medium.com/@tom.truyen/implement-qr-code-scanner-in-android-kotlin-be5fa7f0d50c
+
--- app/src/main/java/com/example/patientapp/MainActivity.kt
+++ app/src/main/java/com/example/patientapp/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.patientapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
 import android.view.View
 import android.widget.*
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
@@ -17,6 +21,8 @@ class MainActivity : AppCompatActivity(),
     AdapterView.OnItemSelectedListener {
     var sunSign = "100001"
     var resultView: TextView? = null
+    private lateinit var mQrResultLauncher : ActivityResultLauncher<Intent>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +48,23 @@ class MainActivity : AppCompatActivity(),
 
         resultView = findViewById(R.id.resultView)
 
+        // QR Code
+        mQrResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if(it.resultCode == Activity.RESULT_OK) {
+                val result = IntentIntegrator.parseActivityResult(it.resultCode, it.data)
+
+                if(result.contents != null) {
+                    // Do something with the contents (this is usually a URL)
+                    println(result.contents)
+                }
+            }
+        }
+
+        // Fire up the camera to scan QR code
+        val button: Button = findViewById(R.id.openCamera)
+        button.setOnClickListener {
+            startScanner()
+        }
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -129,5 +152,16 @@ class MainActivity : AppCompatActivity(),
 
     private fun setText(text: TextView?, value: String) {
         runOnUiThread { text!!.text = value }
+    }
+
+    // Start the QR Scanner
+    private fun startScanner() {
+        val scanner = IntentIntegrator(this)
+        // QR Code Format
+        scanner.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        // Set Text Prompt at Bottom of QR code Scanner Activity
+        scanner.setPrompt("QR Code Scanner Prompt Text")
+        // Start Scanner (don't use initiateScan() unless if you want to use OnActivityResult)
+        mQrResultLauncher.launch(scanner.createScanIntent())
     }
 }
